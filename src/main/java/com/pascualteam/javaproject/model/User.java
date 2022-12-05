06eb366515867f302d201c148a24a6441e2646a8
Álvaro Urda <alvaro.urda@solera.com>
2022-12-05 13:55:29 +0100

    Hotfix

--- src/main/java/com/pascualteam/javaproject/model/User.java
+++ src/main/java/com/pascualteam/javaproject/model/User.java
@@ -1,10 +1,12 @@
 package com.pascualteam.javaproject.model;
 import io.swagger.annotations.ApiModel;
 import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import org.springframework.data.annotation.Id;
 
+@Entity
 public class User {
 
     @Id
